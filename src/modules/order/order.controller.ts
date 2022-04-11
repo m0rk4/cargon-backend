bcb041349a575290be96268b22ba58d56f481743
newvlad2001 <newvlad2001@gmail.com>
2022-04-11 23:26:02 +0300

    little fix

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -27,11 +27,6 @@ export class OrderController {
     return this.orderService.getDriverOrders(+driverId);
   }
 
-  @Get('driver-orders/:driver_id')
-  async getDriverOrders(@Param('driver_id') driver_id: string) {
-    return this.orderService.getDriverOrders(+driver_id);
-  }
-
   @Get(':id')
   async getOrder(@Param('id') id: string) {
     return this.orderService.getOrder(+id);
